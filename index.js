8ac1f9c14fc263bd65ec5b970f004cef697e3a7c
Andrew T Martin III <atmartin93@live.com>
2017-10-17 21:56:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  return string.toLowerCase()
-}
+  if (string.toLowercase()===string) {
+  return "I can't hear you!"  
+  }    
+} 
